dbea679999c2c520a76affdec541898b3e12108a
Brennan Macaig <bmacaig@mutualink.net>
2020-12-02 11:32:43 -0500

    Added part2, left both solutions sloppy

--- day2.py
+++ day2.py
@@ -3,6 +3,7 @@ with open("input/day2.txt") as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 
+## part 1:
 # content[i].split = ['n-k', 'c:', 'password']
 total = 0
 for line in content:
@@ -17,3 +18,15 @@ for line in content:
     if count <= int(r[1]) and count >= int(r[0]):
         total += 1
 print("total: " + str(total))
+
+## part 2:
+total = 0
+for line in content:
+    count = 0
+    l = line.split()
+    r = l[0].split('-') # r[0] is low, r[1] is high
+    c = l[1][0]
+    p = l[2]
+    if (p[int(r[0])-1] == c and not p[int(r[1])-1] == c) or (not p[int(r[0])-1] == c and p[int(r[1])-1] == c): # xor
+        total += 1
+print("part2 total: " + str(total))
